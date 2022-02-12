d572a03451efebaa14b1a37af880e5d073e56506
James Tomasino <james@tomasino.org>
2022-02-12 14:01:12 +0000

    adds tests

---
+++ index.test.js
@@ -0,0 +1,72 @@
+const { JSDOM } = require("jsdom");
+const dom = new JSDOM(`<!DOCTYPE html>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+<p>Hello world</p>
+`);
+
+const originalWindow = { ...window }
+const windowSpy = jest.spyOn(global, 'window', 'get')
+const { addScrollListener, removeScrollListener } = require('./index');
+const waitRAF = () => new Promise(resolve => requestAnimationFrame(resolve))
+
+describe('When addScrollListener adds a callback', () => {
+  const mockCallback = jest.fn()
+  addScrollListener(mockCallback)
+
+  it('it should fire on scroll', async () => {
+    windowSpy.mockImplementation(() => ({
+      ...originalWindow,
+      pageYOffset: -40
+    }))
+    await waitRAF();
+    expect(mockCallback).toHaveBeenCalled()
+  })
+
+  it('it shouldn\'t fire twice for a single scroll', async () => {
+    await waitRAF();
+    expect(mockCallback).toHaveBeenCalledTimes(1)
+  })
+
+  it('it should fire on subsequent scrolls', async () => {
+    windowSpy.mockImplementation(() => ({
+      ...originalWindow,
+      pageYOffset: 0
+    }))
+    await waitRAF();
+    expect(mockCallback).toHaveBeenCalledTimes(2)
+  })
+})
+
+describe('When removeScrollListener removes a callback', () => {
+  const mockCallback = jest.fn()
+  addScrollListener(mockCallback)
+  removeScrollListener(mockCallback)
+
+  it('it shouldn\'t fire on scroll', async () => {
+    windowSpy.mockImplementation(() => ({
+      ...originalWindow,
+      pageYOffset: -40
+    }))
+    await waitRAF();
+    expect(mockCallback).toHaveBeenCalledTimes(0)
+  })
+})
